34eae0f0755cf2058b6031a5c08fdc9c4eb18b03
bonsk5852 <bonsk5852@HRH-IC0027300.wrdsb.ca>
2017-09-11 13:06:32 -0400

    1-9 complete

---
+++ src/A1Q1/A1Q10.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package A1Q1;
+
+/**
+ *
+ * @author bonsk5852
+ */
+public class A1Q10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
--- src/A1Q1/A1Q9.java
+++ src/A1Q1/A1Q9.java
@@ -4,6 +4,8 @@
  */
 package A1Q1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author bonsk5852
@@ -14,6 +16,25 @@ public class A1Q9 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Create a Scanner
+        Scanner input = new Scanner(System.in);
+        // Create an array
+        String words[] = new String[10];
+        // Get 10 words from the user and put them in the array
+        System.out.println("Please input 10 words");
+        for (int i = 0; i < words.length; i++){
+            words[i] = input.nextLine();
+        }
+        // Create a variable for the total number of characters in all the words
+        int total = 0;
+        // Find the total number of characters in all the words
+        for(int i = 0; i <words.length; i++){
+            total = total + words[i].length();
+        }
+        System.out.println("The total number of characters in all the words is " + total + ".");
+        
+        // Divide it by the number of words to calculate the average and output the result
+        double average = total/words.length;
+        System.out.println("The average length is " + average);
     }
 }
